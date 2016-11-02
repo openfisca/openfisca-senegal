cb482a18612e404829bf8cfb7a8b8bd86f68d9b7
Christophe Benz <christophe.benz@gmail.com>
2016-11-02 19:58:42 +0000

    Implement tax and benefit system from notebook

--- openfisca_senegal/senegal_taxbenefitsystem.py
+++ openfisca_senegal/senegal_taxbenefitsystem.py
@@ -12,8 +12,8 @@ COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class SenegalTaxBenefitSystem(TaxBenefitSystem):
     """Senegalese tax and benefit system"""
+    CURRENCY = u"FCFA"
 
     def __init__(self):
-        TaxBenefitSystem.__init__(self, entities.entities)
+        super(SenegalTaxBenefitSystem, self).__init__(entities.entities)
         self.Scenario = scenarios.Scenario
-        self.add_variables_from_file(os.path.join(COUNTRY_DIR, 'model.py'))
