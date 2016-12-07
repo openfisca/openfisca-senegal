7de6eba3d4f01f4d8fbb06f33ddee3e2c1f38c98
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-12-07 17:37:05 +0100

    Run a simulation

---
+++ openfisca_senegal/tests/test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from openfisca_senegal import CountryTaxBenefitSystem as SenegalTBS
+
+tax_benefit_system = SenegalTBS()
+
+scenario = tax_benefit_system.new_scenario()
+
+scenario.init_single_entity(
+    parent1={
+        'salaire': 1800000,
+        'est_marie': True,
+        'conjoint_a_des_revenus': False,
+        'nombre_enfants': 2,
+    },
+    period='2015',
+)
+
+simulation = scenario.new_simulation()
+
+print('Salaire retenu :')
+print(simulation.individu('salaire', period='2015'))
+print('Impot avant réduction famille :')
+print(simulation.individu('impot_avant_reduction_famille', period='2015'))
+print('Réduction pour charge famille :')
+print(simulation.individu('reduction_impots_pour_charge_famille', period='2015'))
+print('Réduction pour charge famille :')
+print(simulation.individu('impot_revenus', period='2015'))
