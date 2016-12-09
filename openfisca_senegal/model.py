6796a29adc733e86cd5513efbb201ab75adb14f5
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-12-09 15:34:33 +0100

    Fix impot_revenus et nombre de parts and test

--- openfisca_senegal/model.py
+++ openfisca_senegal/model.py
@@ -57,11 +57,13 @@ class pension_retraite(Variable):
     label = u"Pension Retraite"
     set_input = set_input_divide_by_period
 
+
 class benefices_non_salarie(Variable):
     column = FloatCol
     entity = Individu
     label = u"Bénéfices non salarié"
     set_input = set_input_divide_by_period
+
 
 class nombre_de_parts(Variable):
     column = FloatCol
@@ -70,11 +72,11 @@ class nombre_de_parts(Variable):
 
     def function(individu, period):
         nombre_de_parts_enfants = individu('nombre_enfants', period) * 0.5
-
         conjoint_a_des_revenus = individu('conjoint_a_des_revenus', period)
         est_marie = individu('est_marie', period)
-        nombre_de_parts_conjoint = est_marie * 0.5 + (1 - conjoint_a_des_revenus) * 0.5
-
+        nombre_de_parts_conjoint = est_marie * (
+            est_marie * 0.5 + (1 - conjoint_a_des_revenus) * 0.5
+            )
         nombre_de_parts = 1 + nombre_de_parts_conjoint + nombre_de_parts_enfants
 
         return period, min_(5, nombre_de_parts)
@@ -86,24 +88,20 @@ class impot_avant_reduction_famille(Variable):
 
     def function(individu, period, legislation):
         salaire = individu('salaire', period, options = [ADD])
-        salaire_abattement = 0.3 * salaire
+        salaire_abattement = min_(0.3 * salaire, 900000)
         salaire_imposable = salaire - salaire_abattement
 
         pension_retraite = individu('pension_retraite', period, options = [ADD])
-        pension_abbattement = max_(pension_retraite * 0.4, 1800000)
+        pension_abbattement = max_(pension_retraite * 0.4, 1800000) * (pension_retraite > 0)
         retraite_imposable = pension_retraite - pension_abbattement
-
         benefices_non_salarie = individu('benefices_non_salarie', period, options = [ADD])
-        benefice_abattement = benefices_non_salarie*0.15
+        benefice_abattement = benefices_non_salarie * 0.15
         benefices_imposable = benefices_non_salarie - benefice_abattement
 
         revenus_arrondis = floor_divide(salaire_imposable + retraite_imposable + benefices_imposable, 1000) * 1000
         revenus_imposable = max_(0, revenus_arrondis)
-        
-        revenus_imposable = salaire_imposable + retraite_imposable + benefices_imposable
 
         bareme_impot_progressif = legislation(period).bareme_impot_progressif
-
         return period, bareme_impot_progressif.calc(revenus_imposable)
 
 
@@ -116,6 +114,11 @@ class reduction_impots_pour_charge_famille(Variable):
 
         nombre_de_parts = individu('nombre_de_parts', period)
         reductions_pour_charge_de_famille = legislation(period).reductions_pour_charge_de_famille
+
+        print 'min', reductions_pour_charge_de_famille.min_6
+        print 'max', reductions_pour_charge_de_famille.max_6
+        print 'taux', reductions_pour_charge_de_famille.taux_6
+
         taux = (nombre_de_parts == 1) * reductions_pour_charge_de_famille.taux_1 + \
             (nombre_de_parts == 1.5) * reductions_pour_charge_de_famille.taux_2 + \
             (nombre_de_parts == 2) * reductions_pour_charge_de_famille.taux_3 + \
@@ -144,8 +147,14 @@ class reduction_impots_pour_charge_famille(Variable):
             (nombre_de_parts == 4.5) * reductions_pour_charge_de_famille.max_8 + \
             (nombre_de_parts == 5) * reductions_pour_charge_de_famille.max_9
 
+        print 'min', minimum
+        print 'max', maximum
+        print 'taux', taux
+        print impot_avant_reduction_famille * taux
         reduction_impot = clip(impot_avant_reduction_famille * taux, a_min = minimum, a_max = maximum)
-
+        print impot_avant_reduction_famille
+        print reduction_impot
+        print impot_avant_reduction_famille - reduction_impot
         return period, reduction_impot
 
 
