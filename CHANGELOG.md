a10eb19be8495e084f5cf97bf71691380b889bc5
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-01-27 12:06:42 +0100

    Clarify setup.cfg

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.3.1
+
+* Clarify setup.cfg
+
 ## 0.3.0
 
 * Add parameters.xml in MANIFEST.in
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-Senegal',
-    version='0.3.0',
+    version='0.3.1',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     classifiers=[
