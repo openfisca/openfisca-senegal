d03f5001d3c0aabd955946b2416c797e9f3c415c
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-09-27 21:53:29 +0200

    Bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-Senegal',
-    version='0.5.6',
+    version='0.6',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     classifiers=[
