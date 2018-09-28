0f6ed3b3fe56a055a63119125bd8a66d7951d346
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-09-28 10:56:29 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     keywords='benefit microsimulation senegal social tax',
     license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
     url='https://github.com/openfisca/senegal',
-
     data_files=[
         ('share/openfisca/openfisca-senegal',
          ['CHANGELOG.md', 'LICENSE', 'README.md']),
@@ -40,16 +39,17 @@ setup(
             'OpenFisca-Survey-Manager >= 0.9.5',
             ],
         'test': [
-            'flake8',
-            'nose'
+           'flake8 >= 3.4.0, < 3.5.0',
+            'flake8-print',
+            'nose',
             ]
         },
     include_package_data = True,  # Will read MANIFEST.in
     install_requires=[
-        'OpenFisca-Core >= 21.0',
+        'OpenFisca-Core >= 23.1, < 24.0',
         'PyYAML >= 3.10',
         'psutil >= 5.4.6',
         ],
-    packages=find_packages(exclude=['openfisca_senegal.tests*']),
+    packages=find_packages(),
     test_suite='nose.collector',
     )
