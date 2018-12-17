fe821937f6c31d7cf42ed5ed619de2845aeb1ad5
sandcha <sandcha@users.noreply.github.com>
2018-12-17 20:21:20 +0100

    Make flake8 happy

--- openfisca_senegal/entities.py
+++ openfisca_senegal/entities.py
@@ -11,11 +11,12 @@ Menage = build_entity(
     Ménage est un exemple d'entité groupe.
     Une entité groupe contient un ou plusieurs individus.
     Chaque individu dans une entité groupe a un rôle (e.g. parent ou enfant).
-    Certains rôles ne peuvent être tenus que par un nombre limité d'individus (e.g. a 'premier_parent' ne peut être tenu que
-    par un individu), alors que d'autres peuvent avoir un nombre illimité d'individus (e.g. 'enfant').
+    Certains rôles ne peuvent être tenus que par un nombre limité d'individus (e.g. a 'premier_parent' ne peut être
+    tenu que par un individu), alors que d'autres peuvent avoir un nombre illimité d'individus (e.g. 'enfant').
 
     Exemple :
-    Les variables de logement (e.g. 'taxe_habitation') sont généralement définies pour une entité groupe telle que 'Menage'.
+    Les variables de logement (e.g. 'taxe_habitation') sont généralement définies pour une entité groupe
+    telle que 'Menage'.
 
     Utilisation :
     Vérifier le nombre d'individus d'un rôle spécifique (e.g. vérifier s'il y a un 'second_parent') avec
@@ -57,7 +58,7 @@ Individu = build_entity(
     Utilisation :
     Calculer une variable s'appliquant à un 'Individu' (e.g. accéder au 'salaire' d'un mois donné) avec
     individu('salaire', "2017-05").
-    Vérifier le rôle d'un 'Individu' dans une entité groupe (e.g. vérifier si 'Individu' est 'premier_parent' 
+    Vérifier le rôle d'un 'Individu' dans une entité groupe (e.g. vérifier si 'Individu' est 'premier_parent'
     dans une entité 'Menage') avec individu.has_role(Menage.PREMIER_PARENT)).
 
     Pour en savoir plus, consulter ce lien : https://openfisca.org/doc/coding-the-legislation/50_entities.html
