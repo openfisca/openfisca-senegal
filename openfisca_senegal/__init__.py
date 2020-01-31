f7f6b1346fa133a453c148daf997be3e61269391
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-01-31 10:44:46 +0100

    Add coicop option to TBS

--- openfisca_senegal/__init__.py
+++ openfisca_senegal/__init__.py
@@ -16,7 +16,7 @@ COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
 # The name CountryTaxBenefitSystem must not be changed, as all tools of the OpenFisca ecosystem expect a
 # CountryTaxBenefitSystem class to be exposed in the __init__ module of a country package.
 class CountryTaxBenefitSystem(TaxBenefitSystem):
-    def __init__(self):
+    def __init__(self, coicop = False):
         # We initialize our tax and benefit system with the general constructor
         super(CountryTaxBenefitSystem, self).__init__(entities.entities)
 
@@ -26,11 +26,11 @@ class CountryTaxBenefitSystem(TaxBenefitSystem):
         # We add to our tax and benefit system all the legislation parameters defined in the  parameters files
         param_path = os.path.join(COUNTRY_DIR, 'parameters')
         self.load_parameters(param_path)
-        try:
-            from openfisca_ceq.tests.test_indirect_tax_variables_generator import add_coicop_item_to_tax_benefit_system
-            add_coicop_item_to_tax_benefit_system(self, country = "senegal")
-        except (configparser.NoSectionError, ModuleNotFoundError) as e:
-            log.info("No ceq baecause:")
-            log.info(e)
-            log.info("Passing")
-
+        if coicop:
+            try:
+                from openfisca_ceq.tests.test_indirect_tax_variables_generator import add_coicop_item_to_tax_benefit_system
+                add_coicop_item_to_tax_benefit_system(self, country = "senegal")
+            except (configparser.NoSectionError, ModuleNotFoundError) as e:
+                log.info("No coicop consumption variable: \n")
+                log.info(e)
+                log.info("Passing")
